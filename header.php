ceaf35fe9a9f580d5da4b658659cac90641e333e
zeromh1810 <c.hickmann86@gmail.com>
2019-07-17 19:30:57 -0400

    boilerplate para crear temas de wordpress

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo('charset') ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php get_template_part('_includes/iOS', 'icons') ?>
+	<?php wp_head() ?>
+</head>
+<body>
+
+	<?php if ( has_nav_menu( 'header-menu' ) ) { ?>
+		<?php wp_nav_menu( array( 
+			'theme_location' => 'header-menu',
+			'container_class' => 'header-menu'
+		) ); ?>
+	<?php } ?>
